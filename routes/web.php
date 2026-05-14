4354d1732e2df69ad589a6a64a3b4badece56cac
Andreza UAD <2300018344@webmail.uad.ac.id>
2026-05-14 19:54:14 +0700

    Post test Rweb 5

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Routing tanpa parameter
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/about', function () {
+    return "Ini halaman About";
+});
+
+Route::get('/kontak', function () {
+    return "Ini halaman Kontak";
+});
+
+/*
+|--------------------------------------------------------------------------
+| Routing dengan parameter
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/user/{name}', function ($name) {
+    return "Halo, $name";
+});
+
+Route::get('/tambah/{a}/{b}', function ($a, $b) {
+    return "Hasil penjumlahan: " . ($a + $b);
+});
+
+Route::get('/nilai/{nama}/{nilai}', function ($nama, $nilai) {
+
+    if ($nilai >= 75) {
+        $status = "Lulus";
+    } else {
+        $status = "Tidak Lulus";
+    }
+
+    return "Nama: $nama | Nilai: $nilai | Status: $status";
 });
